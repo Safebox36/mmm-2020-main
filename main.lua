8673ab2fb4b822ee71e3a53a7625083accbf4b7e
Safebox36 <safebox36@gmail.com>
2020-12-29 17:57:13 +0000

    Free Warden

--- main.lua
+++ main.lua
@@ -1,29 +1,50 @@
 local bonfire = require("mmm-2020-main\\bonfire\\main")
 
+local function freePrisonersFadeIn()
+    local captive1 = tes3.getReference("AA_hostage01")
+    local captive2 = tes3.getReference("AA_hostage02")
+    local captive3 = tes3.getReference("AA_hostage03")
+    local warden = tes3.getReference("AA_warden")
+    local player = tes3.player
+    captive1.position = captive1.position + captive1.sceneNode.rotation:transpose().y * 128
+    captive2.position = captive2.position + captive2.sceneNode.rotation:transpose().y * 128
+    captive3.position = captive3.position + captive3.sceneNode.rotation:transpose().y * 128
+    warden.position = warden.position + warden.sceneNode.rotation:transpose().y * 128
+    player.position = player.position - player.sceneNode.rotation:transpose().y * 128
+    tes3.fadeIn {duration = 1.0}
+    tes3.updateJournal {id = 'AA_Stormwatch_Hostages', index = 7, showMessage = false}
+end
+
+local function freePrisonersFadeOut()
+    tes3.fadeOut {duration = 1.0}
+    tes3.updateJournal {id = 'AA_Stormwatch_Hostages', index = 6, showMessage = false}
+    timer.start {type = timer.simulate, iterations = 1, duration = 1, callback = freePrisonersFadeIn}
+end
+
 local function unbrokenWall()
-    tes3.getReference("AA_CellBrick0").disabled = true
-    tes3.getReference("AA_CellBrick1").disabled = true
-    tes3.getReference("AA_CellBrick2").disabled = true
-    tes3.getReference("AA_CellBrick3").disabled = true
-    tes3.getReference("AA_CellBrick4").disabled = true
+    tes3.getReference("AA_CellBrick0"):disable()
+    tes3.getReference("AA_CellBrick1"):disable()
+    tes3.getReference("AA_CellBrick2"):disable()
+    tes3.getReference("AA_CellBrick3"):disable()
+    tes3.getReference("AA_CellBrick4"):disable()
 end
 
 local function brokenWall()
-    tes3.getReference("AA_CellBrick0").disabled = false
-    tes3.getReference("AA_CellBrick1").disabled = false
-    tes3.getReference("AA_CellBrick2").disabled = false
-    tes3.getReference("AA_CellBrick3").disabled = false
-    tes3.getReference("AA_CellBrick4").disabled = false
+    tes3.getReference("AA_CellBrick0"):enable()
+    tes3.getReference("AA_CellBrick1"):enable()
+    tes3.getReference("AA_CellBrick2"):enable()
+    tes3.getReference("AA_CellBrick3"):enable()
+    tes3.getReference("AA_CellBrick4"):enable()
 
-    tes3.getReference("AA_PlaneWall").disabled = true
+    tes3.getReference("AA_PlaneWall"):disable()
     tes3.setGlobal("AA_Escarpe", 2)
 end
-
 
 local function sabotageBalista(ref)
     local balistasSabotaged = tes3.getGlobal("AA_balistasSabotaged")
     if (balistasSabotaged) then
         tes3.setGlobal("AA_balistasSabotaged", balistasSabotaged + 1)
+        balistasSabotaged = tes3.getGlobal("AA_balistasSabotaged")
     end
     if (balistasSabotaged < 10) then
         tes3.messageBox{
@@ -43,7 +64,6 @@ end
 local function removePlayerInventory()
     local inv = tes3.player.object.inventory
     for item in tes3.iterate(inv) do
-        print(item.object.id)
 		tes3.transferItem({
 			from = tes3.player,
 			to = tes3.getReference("AA_GuardChest"),
@@ -82,6 +102,9 @@ local function dispActivate(e)
                 }
             end
         end
+        if (e.target.baseObject.id == "TS_dr_dung_cage_03" and tes3.getJournalIndex {id = 'AA_Stormwatch_Hostages'} == 5) then
+            freePrisonersFadeOut()
+        end
     end
 end
 
@@ -100,6 +123,10 @@ local function dispUpdate(e)
             unbrokenWall()
         elseif (tes3.getGlobal("AA_Escarpe") == 1) then
             brokenWall()
+        elseif (tes3.getGlobal("AA_Escarpe") == 2) then
+            if (tes3.getGlobal("AA_chest") == 1 and tes3.getJournalIndex {id = 'AA_Stormwatch_Cult'} == 5) then
+                tes3.updateJournal {id = 'AA_Stormwatch_Cult', index = 10, showMessage = true}
+            end
         end
     end
 end
